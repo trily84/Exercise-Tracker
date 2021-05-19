f7165b5ac260ef8e3578c8c61c4ae806aee4c345
Tri Ly <tmly283@gmail.com>
2021-05-19 17:03:56 -0600

    first commit

---
+++ server.js
@@ -0,0 +1,51 @@
+var express = require('express');
+var app = express();
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/api/:datestring?', (req, res) => {
+    let datestring = req.params.datestring
+    let date
+
+    // if datestring is empty
+    if (!datestring) {
+        date = new Date()
+    }
+    // if datestring is not empty
+    else {
+        // if datestring is a number string
+        if (!isNaN(datestring)) {
+            // convert it to interger
+            datestring = parseInt(datestring)
+            date = new Date(datestring)
+            // res.json({datestring: datestring, typeofdatestring: typeof(datestring), date: typeof(date)})
+        }
+        // if datestring is a string like 2020-01-01
+        else {
+            date = new Date(datestring)
+            // res.json({datestring: datestring, typeofdatestring: typeof(datestring), date: typeof(date)})
+        }
+    }     
+
+    // determine if date is a string or an integer 
+    if (date.toString() === "Invalid Date") {
+        res.json({error: date.toString()})
+    }
+    else {
+        res.json({Unix: date.getTime(), UTC: date.toUTCString()})
+    }
+})
+
+// var listener = app.listen(process.env.PORT || 3000, function () {
+//   console.log('Your app is listening on port ' + listener.address().port);
+// });
+
+// create environment variable to listen to dynamic hosting port
+const port = process.env.PORT || 3000
+app.listen(port, () => console.log("Listening on port " + port + "..."))
