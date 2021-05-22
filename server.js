c72033e11ee68462b3612fd60099952dd7528886
Tri Ly <tmly283@gmail.com>
2021-05-22 15:53:32 -0600

    update node version to 4.7.0 in package.json

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var app = express();
+var app = express()
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
